3c48fe330c1d7339245697f3b7af02e03dd74ee8
Sirsho29 <64305020+Sirsho29@users.noreply.github.com>
2020-10-01 00:03:17 +0530

    multiple inheritance

--- multiple_inheritance.cpp
+++ multiple_inheritance.cpp
@@ -1,1 +1,73 @@
-//WAP
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    A() { cout << "A's constructor called" << endl; }
+};
+
+class B
+{
+public:
+    B() { cout << "B's constructor called" << endl; }
+};
+
+class C : public B, public A
+{
+public:
+    C() { cout << "C's constructor called" << endl; }
+};
+
+int main()
+{
+    C c;
+    return 0;
+}
+
+
+//EXAMPLE OF MULTIPLE INHERITANCE : 
+
+
+// #include <iostream>
+// using namespace std;
+// class Person
+// {
+
+// public:
+//     Person(int x) { cout << "Person::Person(int ) called" << endl; }
+// };
+
+// class Faculty : public Person
+// {
+
+// public:
+//     Faculty(int x) : Person(x)
+//     {
+//         cout << "Faculty::Faculty(int ) called" << endl;
+//     }
+// };
+
+// class Student : public Person
+// {
+
+// public:
+//     Student(int x) : Person(x)
+//     {
+//         cout << "Student::Student(int ) called" << endl;
+//     }
+// };
+
+// class TA : public Faculty, public Student
+// {
+// public:
+//     TA(int x) : Student(x), Faculty(x)
+//     {
+//         cout << "TA::TA(int ) called" << endl;
+//     }
+// };
+
+// int main()
+// {
+//     TA ta1(30);
+// }
